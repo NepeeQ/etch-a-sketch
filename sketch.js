7bda28c2508fe15e4349a54089961e42e82c1c71
NepeeQ <waderan355@gmail.com>
2023-04-01 18:44:20 +0200

    Added rainbow function

--- sketch.js
+++ sketch.js
@@ -2,21 +2,32 @@ const canvas = document.querySelector("#canvas");
 const slider = document.querySelector("#gridsize");
 const sliderLabel = document.querySelector("#squareNum");
 const resetButton = document.querySelector("#reset");
-const colorPick = document.querySelector("#colorPick")
-
+const colorPick = document.querySelector("#colorPick");
+const rainbowButton = document.querySelector("#rainbow");
+const modeInfo = document.querySelector("#modeInf");
 
 const DEFAULT_GRID_SIZE = 16;
 const DEFAULT_BLOCK_SIZE = 40;
 
 let currentColor = "black";
 
+let rainbowSet = false;
 
-
-
+rainbowButton.addEventListener("click", function()
+{
+    rainbowSet = true;
+    console.log(rainbowSet);
+    modeInfo.textContent = " ";
+    
+    modeInfo.appendChild 
+    modeInfo.style.opacity = "100%";
+} )
 
 colorPick.addEventListener("change", function()
 {
     currentColor = colorPick.value;
+    modeInfo.style.opacity = "0%";
+    rainbowSet = false;
 })
 
 resetButton.addEventListener("click", function(){
@@ -65,12 +76,24 @@ function createGrid(num)
 
 function draw()
 {
-    console.log(this);
-    this.style.backgroundColor= currentColor;
+
+    if (rainbowSet === true)
+    {
+        rainbowMode();
+    }
+
+    this.style.backgroundColor = currentColor;
 }
 
 
-
+function rainbowMode (){
+    let color = "#"
+    for (let i = 0; i < 6; i++)
+    {
+        color += Math.floor(Math.random() * 16).toString(16);
+    }
+    currentColor = color;
+}
 
 window.onload = () => {
     createGrid(DEFAULT_GRID_SIZE);
